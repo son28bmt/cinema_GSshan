14aed8dcbd1531ec8c6063397b985dd75a8bba4d
NguyenQuangSon_React_4 <quangsonnguyen2807@gmail.com>
2026-01-09 19:23:41 +0700

    hay

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,6 +1,14 @@
 require("./env");
-
+const fs = require("fs");
+const path = require("path");
 const mysql = require("mysql2/promise");
+const sslCaPath = process.env.DB_SSL_CA_PATH;
+const sslCaRaw = process.env.DB_SSL_CA;
+const sslCa = sslCaRaw ? sslCaRaw.replace(/\\n/g, "\n") : "";
+const ssl =
+  sslCaPath || sslCa
+    ? { ca: sslCaPath ? fs.readFileSync(path.resolve(sslCaPath), "utf8") : sslCa }
+    : undefined;
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
@@ -10,7 +18,8 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME || "cinema",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl
 });
 
 module.exports = pool;
